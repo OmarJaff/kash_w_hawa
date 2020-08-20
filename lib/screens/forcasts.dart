3cd7018bce4a642562ee9e7b6e1b635a03272358
Omar Jaff <omarjaff95@gmail.com>
2020-08-20 10:11:53 +0300

    finished navigating between pages

---
+++ lib/screens/forcasts.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Forcasts extends StatefulWidget {
+  @override
+  _ForcastsState createState() => _ForcastsState();
+}
+
+class _ForcastsState extends State<Forcasts> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('This is forcasts page'),
+    );
+  }
+}
+
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,32 +1,61 @@
 import 'package:KashWHawa/screens/current_weather.dart';
+import 'package:KashWHawa/screens/current_weather.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:KashWHawa/utilities/constants.dart';
-import 'package:KashWHawa/services/weather.dart';
 import 'package:flutter/widgets.dart';
-
+import 'package:KashWHawa/screens/settings.dart';
+import 'package:KashWHawa/screens/forcasts.dart';
 
 class LocationScreen extends StatefulWidget {
   LocationScreen({this.weatherData});
   final dynamic weatherData;
+
   @override
   _LocationScreenState createState() => _LocationScreenState();
 }
 
 class _LocationScreenState extends State<LocationScreen> {
-  WeatherModel weather = WeatherModel();
-  var tempreture;
-  String weatherDescription;
-  String weatherIcon;
-  String cityName;
+  int _pageIndex = 0;
+  PageController _pageController;
+  List<Widget> tabPages = [
+    CurrentWeather(),
+    Settings(),
+    Forcasts()
+  ];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _pageController = PageController(initialPage: _pageIndex);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void onPageChange(int page) {
+    setState(() {
+      this._pageIndex = page;
+    });
+  }
+
+  void onTabTapped(int index) {
+    this._pageController.animateToPage(index,duration: const Duration(milliseconds: 500),curve: Curves.easeInOut);
+  }
 
   @override
 
   Widget build(BuildContext context) {
     return Scaffold(
-      body:  CurrentWeather(),
+
       bottomNavigationBar: 
       BottomNavigationBar(
+        currentIndex: _pageIndex,
+            onTap: onTabTapped,
             backgroundColor: Colors.white,
             selectedItemColor: kActiveItemColor,
 
@@ -49,6 +78,10 @@ class _LocationScreenState extends State<LocationScreen> {
               title: Text('Settings'),
             ),
       ]),
+        body:  PageView(children: tabPages,
+      onPageChanged: onPageChange,
+      controller: _pageController,
+    ),
     );
   }
 }
