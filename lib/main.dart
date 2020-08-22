733895d6a4bddea9d685a4b9910e727495ac6871
Omar Jaff <omarjaff95@gmail.com>
2020-08-22 11:33:24 +0300

    forced the app not to support horizantal oreintaion

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import './utilities/constants.dart';
 import 'package:flutter/material.dart';
 import './screens/loading_screen.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       theme: ThemeData.light().copyWith(
       primaryColor: kPrimaryColor,
           scaffoldBackgroundColor:Color(0xFFE5F0FC),
           unselectedWidgetColor:kPrimaryColor
-
       ),
       home: LoadingScreen(),
     );
